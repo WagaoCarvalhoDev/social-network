9bde5ea30c45083c4662ea61cb4bfd5f3166ac5f
WagaoCarvalhoDev <wagaocarvalhodev@gmail.com>
2024-01-22 21:13:30 -0300

    add: main.go NewAPIRouter ListenAndServe

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"backend/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("teste")
+
+	r := router.NewAPIRouter()
+	log.Fatal(http.ListenAndServe(":5000", r))
 }
