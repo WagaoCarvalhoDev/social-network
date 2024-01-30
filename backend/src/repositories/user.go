1e73847466254eb818141db1372151fdf5cdc32f
WagaoCarvalhoDev <wagaocarvalhodev@gmail.com>
2024-01-29 21:34:05 -0300

    add: backend/src/repositories/user.go implement CreateUser

--- backend/src/repositories/user.go
+++ backend/src/repositories/user.go
@@ -14,5 +14,22 @@ func NewUsersRepository(db *sql.DB) *users {
 }
 
 func (u users) CreateUser(user models.User) (uint64, error) {
-	return 0, nil
+	insertUser := "insert into users (nameUser, nick, email, passwd) values(?, ?, ?, ?)"
+	statement, err := u.db.Prepare(insertUser)
+	if err != nil {
+		return 0, err
+	}
+	defer statement.Close()
+
+	result, err := statement.Exec(user.NameUser, user.Nick, user.Email, user.Passwd)
+
+	if err != nil {
+		return 0, err
+	}
+
+	lastInsertId, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return uint64(lastInsertId), nil
 }
